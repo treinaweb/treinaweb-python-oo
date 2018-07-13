d8f8335b2fcceb71c91dfdad8c52881850cab570
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-07-13 16:10:38 -0300

    06 - Métodos com parâmetros

--- carro.py
+++ carro.py
@@ -1,14 +1,15 @@
 class Carro():
-	def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado):
+	def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado, velocidade):
 		self.cor = cor
 		self.qtd_portas = qtd_portas
 		self.tipo_combustivel = tipo_combustivel
 		self.potencia = potencia
 		self.qtd_combustivel = qtd_combustivel
 		self.is_ligado = is_ligado
+		self.velocidade = velocidade
 
-	def abastecer(self):
-		self.qtd_combustivel += 20
+	def abastecer(self, qtd_combustivel):
+		self.qtd_combustivel += qtd_combustivel
 
 	def ligar(self);
 		if self.is_ligado:
@@ -21,3 +22,9 @@ class Carro():
 			print("O carro já está desligado")
 		else:
 			self.is_ligado = False
+
+	def acelerar(self, velocidade=10):
+		if self.is_ligado:
+			self.velocidade += velocidade
+		else:
+			print("O carro precisa estar ligado para ser acelerado")
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import carro
 
-uno_vermelho = carro.Carro("vermelho", 4, "Flex", 1.0, 0, False)
+uno_vermelho = carro.Carro("vermelho", 4, "Flex", 1.0, 0, False, 0)
 uno_vermelho.ligar()
-uno_vermelho.abastecer()
-uno_vermelho.abastecer()
+uno_vermelho.abastecer(50)
+uno_vermelho.abastecer(10)
 print(f"A quantidade de combustível do carro é: {uno_vermelho.qtd_combustivel}")
 
-uno_preto = carro.Carro("preto", 2, "Flex", 1.4, 0, False)
+uno_preto = carro.Carro("preto", 2, "Flex", 1.4, 0, False, 0)
 uno_preto.desligar()
 print(f"A quantidade de combustível do carro é: {uno_preto.qtd_combustivel}")
+uno_preto.acelerar(20)
+uno_preto.ligar()
+uno_preto.acelerar()
+print(uno_preto.velocidade)
