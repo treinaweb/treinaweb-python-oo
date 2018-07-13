e37dc38ab6a352eac3a109ee8f2be9c2cc5a5226
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-07-13 16:32:06 -0300

    12 - Visibilidade public

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ uno_vermelho = carro.Carro("vermelho", 4, "Flex", 1.0, 4)
 uno_vermelho.ligar()
 uno_vermelho.abastecer(50)
 uno_vermelho.abastecer(10)
+uno_vermelho.acelerar(20)
+print(uno_vermelho.velocidade)
 print(f"A quantidade de combustível do carro é: {uno_vermelho.qtd_combustivel}")
 
 moto_vermelha = moto.Moto("Vermelha", "Gasolina", 1.0, 2)
