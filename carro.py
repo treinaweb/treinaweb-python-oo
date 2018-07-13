2a07a4ec03cdb325d23b1ae4661fcaa50f270c9f
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-07-13 15:55:54 -0300

    03 - Criando atributos

--- carro.py
+++ carro.py
@@ -1,2 +1,6 @@
 class Carro():
-	
+	def __init__(self, cor, qtd_portas, tipo_combustivel, potencia):
+		self.cor = cor
+		self.qtd_portas = qtd_portas
+		self.tipo_combustivel = tipo_combustivel
+		self.potencia = potencia
