62f42122d4aa0c1972755d625032c447994a6c04
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-07-13 16:33:58 -0300

    13 - Visibilidade private

--- veiculo.py
+++ veiculo.py
@@ -1,35 +1,34 @@
 class Veiculo():
 	"""Essa é a classe carro. Esta classe é utilizada para instanciar novos carros em nosso programa"""
-	def __init__(self, cor, qtd_portas, tipo_combustivel, potencia):
-		self.cor = cor
-		self.qtd_portas = qtd_portas
-		self.tipo_combustivel = tipo_combustivel
-		self.potencia = potencia
-		self.qtd_combustivel = 0
-		self.is_ligado = False
-		self.velocidade = 0
+	def __init__(self, cor, tipo_combustivel, potencia):
+		self.__cor = cor
+		self.__tipo_combustivel = tipo_combustivel
+		self.__potencia = potencia
+		self.__qtd_combustivel = 0
+		self.__is_ligado = False
+		self.__velocidade = 0
 
 	def __del__(self):
 		print("O objeto foi removido da memória. :)")
 
 	def abastecer(self, qtd_combustivel):
 		"""O método abastecer recebe como parâmetro a quantidade de combustível e incrementa no atributo qtd_combustivel do objeto carro"""
-		self.qtd_combustivel += qtd_combustivel
+		self.__qtd_combustivel += qtd_combustivel
 
 	def ligar(self);
-		if self.is_ligado:
+		if self.__is_ligado:
 			print("O carro já está ligado")
 		else:
-			self.is_ligado = True
+			self.__is_ligado = True
 
 	def desligar(self):
-		if self.is_ligado == False:
+		if self.__is_ligado == False:
 			print("O carro já está desligado")
 		else:
 			self.is_ligado = False
 
 	def acelerar(self, velocidade=10):
-		if self.is_ligado:
-			self.velocidade += velocidade
+		if self.__is_ligado:
+			self.__velocidade += velocidade
 		else:
 			print("O carro precisa estar ligado para ser acelerado")
